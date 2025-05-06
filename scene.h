175db92bb623ab353b76a5cae160d8d32371f4a5
matfumis <matfumis@gmail.com>
2025-05-06 11:49:54 +0200

    Added scene.h

---
+++ scene.h
@@ -0,0 +1,47 @@
+#ifndef _SCENE_H
+#define _SCENE_H
+
+#include <stdio.h>
+
+struct _pixel{
+  u_int8_t r;
+  u_int8_t g;
+  u_int8_t b;
+}; 
+
+typedef struct _pixel pixel;
+typedef struct _pixel * pixel_ptr;
+
+struct _vector{
+  float x;
+  float y;
+  float z;
+};
+
+typedef struct _vector vector;
+typedef struct _vector * vector_ptr;
+
+struct _sphere {
+  vector center;
+  u_int8_t radius;
+  pixel color;
+};
+
+typedef struct _sphere sphere;
+typedef struct _sphere * sphere_ptr;
+
+struct _scene {
+  vector viewport_size;
+  pixel bg_color; 
+  int sphere_count;
+  FILE * fd;
+  sphere_ptr spheres;
+};
+
+typedef struct _scene scene;
+typedef struct _scene * scene_ptr;
+
+int open_scene_file(char *path, scene_ptr scene);
+pixel_ptr render_image(scene_ptr scene, int width, int height);
+
+#endif
