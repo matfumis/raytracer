c894d31afa75575fe0d2658ef5164aa3bd41dd98
matfumis <matfumis@gmail.com>
2025-07-02 16:01:30 +0200

    Minor refactor

--- main.c
+++ main.c
@@ -6,9 +6,6 @@
 #include "scene.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
-#include <time.h>
-
 /*
 Function main()
 
--- ppm.h
+++ ppm.h
@@ -5,11 +5,7 @@
 #ifndef _PPM_H
 #define _PPM_H
 
-#include <stdio.h>
 #include "scene.h"
-#include <string.h>
-#include <sys/mman.h>
-#include <unistd.h>
 
 int save_image_as_ppm(char *filename, pixel_ptr image, int width, int height);
 
